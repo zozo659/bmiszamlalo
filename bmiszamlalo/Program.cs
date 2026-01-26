9bf7cd35306a9c84560e583a7620b0140a233f51
Kalasziz <Kalasziz@310-08>
2026-01-26 08:11:49 +0100

    legmagasabb feladat

--- bmiszamlalo/Program.cs
+++ bmiszamlalo/Program.cs
@@ -5,8 +5,36 @@
     {
         static void Main(string[] args)
         {
-        List<diak> list = new List<diak>();
+            List<diak> list = new List<diak>();
+            var sorok = File.ReadAllLines("bmi.txt" ,System.Text.Encoding.Latin1).Skip(1);
+            foreach (var sor in sorok)
 
+            {
+                //Console.WriteLine(sor);
+                string[] darabok = sor.Split(';');
+                string nev = darabok[0];
+                int eletkor = Convert.ToInt32(darabok[1]);
+                int magassag = Convert.ToInt32(darabok[2]);
+                int suly = Convert.ToInt32(darabok[3]);
+                diak d = new diak(nev, eletkor, magassag, suly);
+                list.Add(d);
+            }
+            foreach (var d in list)
+            {
+                Console.WriteLine(d);
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine($"3. a, feladat: A diákok száma: {list.Count}");
+
+            diak legmagasabb=list[0];
+            foreach (var diak in list)
+            {
+                if (diak.Magasság > legmagasabb.Magasság)
+                {
+                    legmagasabb = diak;
+                }
+                Console.WriteLine($"3. b, feladat: A legmagasabb diák:" + $"{legmagasabb.Név }, magasság:{legmagasabb.Magasság} cm");
+            }
         }
     }
 }
--- bmiszamlalo/diak.cs
+++ bmiszamlalo/diak.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace bmiszamlalo
 {
-    internal class diak
+    /*Név;Életkor;Magasság;Testsúly
+Kiss Péter;16;175;68
+    */
+    public class diak
     {
+        public string Név { get; set; }
+        public int Életkor { get; set; }
+        public int Magasság { get; set; }
+        public int Testsúly { get; set; }
+
+        public diak(string név, int életkor, int magasság, int testsúly)
+        {
+            Név = név;
+            Életkor = életkor;
+            Magasság = magasság;
+            Testsúly = testsúly;
+        }
+        public override string? ToString()
+        {
+            return $"{Név} {Életkor}éves {Magasság}cm {Testsúly}kg";
+        }
     }
+
 }
